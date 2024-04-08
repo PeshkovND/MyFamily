2cc6f54d22b149133819145e7ee201b047ea0e70
Nikita Peshkov <peshkov.nd@gmail.com>
2024-04-08 12:39:27 +0600

    add: purchase manager

---
+++ XcodeProject/Packages/AppModulesPackage/Sources/AppServices/Sources/PurchaseManager/PurchaseManager.swift
@@ -0,0 +1,75 @@
+//
+
+import Foundation
+import StoreKit
+
+public class PurchaseManager {
+    
+    private var updates: Task<Void, Never>? = nil
+    private(set) var purchasedProductIDs = Set<String>()
+    
+    var hasUnlockedPro: Bool {
+        return !self.purchasedProductIDs.isEmpty
+    }
+    
+    init() {
+        updates = observeTransactionUpdates()
+    }
+    
+    deinit {
+        updates?.cancel()
+    }
+    
+    public func getProducts(names: [String]) async throws -> [Product] {
+        return try await Product.products(for: names)
+    }
+    
+    public func purchase(_ product: Product, completionHandler: () -> Void) async throws {
+        let result = try await product.purchase()
+        
+        switch result {
+        case let .success(.verified(transaction)):
+            // Successful purhcase
+            await transaction.finish()
+            await updatePurchasedProducts()
+            completionHandler()
+        case let .success(.unverified(_, error)):
+            // Successful purchase but transaction/receipt can't be verified
+            // Could be a jailbroken phone
+            break
+        case .pending:
+            // Transaction waiting on SCA (Strong Customer Authentication) or
+            // approval from Ask to Buy
+            break
+        case .userCancelled:
+            // ^^^
+            break
+        @unknown default:
+            break
+        }
+    }
+    
+    func updatePurchasedProducts() async {
+        for await result in Transaction.currentEntitlements {
+            guard case .verified(let transaction) = result else {
+                continue
+            }
+            
+            if transaction.revocationDate == nil {
+                self.purchasedProductIDs.insert(transaction.productID)
+            } else {
+                self.purchasedProductIDs.remove(transaction.productID)
+            }
+        }
+    }
+    
+    private func observeTransactionUpdates() -> Task<Void, Never> {
+        Task(priority: .background) { [unowned self] in
+            for await verificationResult in Transaction.updates {
+                // Using verificationResult directly would be better
+                // but this way works for this tutorial
+                await self.updatePurchasedProducts()
+            }
+        }
+    }
+}
